a541290153e88ea3e78de984b8673e5942b438d6
NiklasKaulfers <niklas.kaulfers@zeiss.com>
2025-05-09 09:19:08 +0200

    gruppe 1 + 2 (test)

---
+++ group1/data/README.md
@@ -0,0 +1,5 @@
+# Data Ordner ReadMe
+
+Hier kommen die ganzen Daten hin  
+In unserem Fall sind das die CSV-Daten vom Calliope und Bilder, welche ihr auf der Webseite nutzen wollt  
+Für Bilder verwenden wir das File Format JPG, also achtet darauf, dass eure Bilder mit .jpg enden.
---
+++ group1/index.js
@@ -0,0 +1,13 @@
+import generateChart from "./lib/generate-chart.js";
+
+
+// Nutze das für deinen Chart!
+
+// async function loadCharts(){
+// await generateChart({
+//     csvFilename: "./data/<dein-csv-file>.csv",
+//     divElement: document.getElementById("<div-des-charts-im-index.html>"),
+//     }
+// )
+// }
+// loadCharts();
